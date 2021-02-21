d2b69949b14470c3d2cadc0849083d036e57c4bb
AdiliaZ <65673503+AdiliaZ@users.noreply.github.com>
2021-02-21 11:45:47 +0300

    FirstConsoleApp

---
+++ MyFirstConsoleApp/MyFirstConsoleApp/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace MyFirstConsoleApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection service)
+        {
+
+        }
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+               {
+                   await context.Response.WriteAsync("Hello Kitty");
+               });
+            });
+        }
+    }
+}
